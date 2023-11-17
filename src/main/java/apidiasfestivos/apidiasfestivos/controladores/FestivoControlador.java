94ea56e1f755e4cef8f1b877128ede116c170093
marco osorio <marco.osorio86@hotmail.com>
2023-11-16 19:00:46 -0500

    ¿?

--- src/main/java/apidiasfestivos/apidiasfestivos/controladores/FestivoControlador.java
+++ src/main/java/apidiasfestivos/apidiasfestivos/controladores/FestivoControlador.java
@@ -1,13 +1,16 @@
 package apidiasfestivos.apidiasfestivos.controladores;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import apidiasfestivos.apidiasfestivos.entidades.FestivoDto;
 import apidiasfestivos.apidiasfestivos.interfaces.IFestivoServicio;
 
 @RestController // Indica que es el controlador
@@ -16,6 +19,7 @@ public class FestivoControlador {
     @Autowired
     private IFestivoServicio servicio;
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/verificar/{año}/{mes}/{dia}", method = RequestMethod.GET)
     public String verificarFestivo(@PathVariable int año, @PathVariable int mes, @PathVariable int dia) {
         if (servicio.esFechaValida(String.valueOf(año) + "-" + String.valueOf(mes) + "-" + String.valueOf(dia))) {
@@ -24,6 +28,12 @@ public class FestivoControlador {
         } else {
             return "Fecha no válida";
         }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/obtener/{año}", method = RequestMethod.GET)
+    public List<FestivoDto> obtener(@PathVariable int año) {
+        return servicio.obtenerFestivos(año);
     }
 
 }
--- src/main/java/apidiasfestivos/apidiasfestivos/entidades/FestivoDto.java
+++ src/main/java/apidiasfestivos/apidiasfestivos/entidades/FestivoDto.java
@@ -2,26 +2,36 @@ package apidiasfestivos.apidiasfestivos.entidades;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class FestivoDto {
     private String festivo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date fecha;
+
     public FestivoDto() {
     }
+
     public FestivoDto(String festivo, Date fecha) {
         this.festivo = festivo;
         this.fecha = fecha;
     }
+
     public String getFestivo() {
         return festivo;
     }
+
     public void setFestivo(String festivo) {
         this.festivo = festivo;
     }
+
     public Date getFecha() {
         return fecha;
     }
+
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
-    
+
 }
